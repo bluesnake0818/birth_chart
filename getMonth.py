6b5acb0de115c5e75b060c9ae9ec61f8f7df0dfd
bluesnake0818 <raidient87@gmail.com>
2022-04-21 17:40:44 -0400

    complete 2-4 months in getMonth

--- getMonth.py
+++ getMonth.py
@@ -6,28 +6,149 @@ month_gan_idx = 0
 month_ji_idx = 0
 
 def getMonth (year_gan, month, date, hour):
-  if(month % 12) == 0: 
+  if(month % 12) == 2: 
+    month_ji_idx = 2
     if(year_gan == 'GAP' or year_gan == 'KII'):
       month_gan_idx = 2
-      month_ji_idx = 2
-      month_gan = 'BYU'
-      month_ji = 'TIG'
       if(date <= 2):
         month_gan_idx -= 1
         month_ji_idx -= 1
-        month_gan = 'EUL'
-        month_ji = 'OXX'
       if(date == 3):
         if(hour < 23):
           month_gan_idx -= 1
           month_ji_idx -= 1
-          month_gan = 'EUL'
-          month_ji = 'OXX'
     elif(year_gan == 'EUL' or year_gan == 'KYU'): 
-      
+      month_gan_idx = 4
+      if(date <= 2):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 3):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
     elif(year_gan == 'BYU' or year_gan == 'SHI'): 
+      month_gan_idx = 6
+      if(date <= 2):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 3):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
     elif(year_gan == 'JUN' or year_gan == 'YIM'): 
+      month_gan_idx = 8
+      if(date <= 2):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 3):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
     else: # If (year_gan == 'MUU' or year_gan == 'KYE')
+      month_gan_idx = 0
+      if(date <= 2):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 3):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+
+  elif(month % 12) == 3: 
+    month_ji_idx = 3
+    if(year_gan == 'GAP' or year_gan == 'KII'):
+      month_gan_idx = 3
+      if(date <= 4):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 5):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'EUL' or year_gan == 'KYU'): 
+      month_gan_idx = 5
+      if(date <= 4):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 5):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'BYU' or year_gan == 'SHI'): 
+      month_gan_idx = 7
+      if(date <= 4):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 5):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'JUN' or year_gan == 'YIM'): 
+      month_gan_idx = 9
+      if(date <= 4):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 5):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    else: # If (year_gan == 'MUU' or year_gan == 'KYE')
+      month_gan_idx = 1
+      if(date <= 4):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 5):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+
+  elif(month % 12) == 4: 
+    month_ji_idx = 4
+    if(year_gan == 'GAP' or year_gan == 'KII'):
+      month_gan_idx = 4
+      if(date <= 3):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 4):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'EUL' or year_gan == 'KYU'): 
+      month_gan_idx = 6
+      if(date <= 3):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 4):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'BYU' or year_gan == 'SHI'): 
+      month_gan_idx = 8
+      if(date <= 3):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 4):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    elif(year_gan == 'JUN' or year_gan == 'YIM'): 
+      month_gan_idx = 1
+      if(date <= 3):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 4):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
+    else: # If (year_gan == 'MUU' or year_gan == 'KYE')
+      month_gan_idx = 3
+      if(date <= 3):
+        month_gan_idx -= 1
+        month_ji_idx -= 1
+      if(date == 4):
+        if(hour < 23):
+          month_gan_idx -= 1
+          month_ji_idx -= 1
 
 
   month_gan_idx = GAN.index(month_gan)
